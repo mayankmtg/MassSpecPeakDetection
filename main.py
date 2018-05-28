2900411f15f8c414e7be9a3010f0866c7ec132be
mayankmtg <mayank15056@iiitd.ac.in>
2018-05-28 19:21:19 +0530

    sorted based on mz

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from mzxmlReader import mzxmlReader as mzx
 from scan import scan
+import matplotlib.pyplot as plt
 
 file_obj=mzx('samples/file1.mzXML')
 xml_scans=file_obj.getScans()
 scan_arr=[]
 
+# scan arr contains all the scans of the class scan python
 for s in xml_scans:
 	scan_obj=scan(s)
 	peaks=s.getElementsByTagName('peaks')
@@ -14,4 +16,24 @@ for s in xml_scans:
 	scan_obj.setPeakPairs(inms_pair)
 	scan_arr.append(scan_obj)
 
-print(len(scan_arr))
+print("Scans Loaded")
+in_mz_rt=[]
+for s in scan_arr:
+	for p in s.peaks:
+		newTup=(p[0], p[1], s.retentionTime)
+		in_mz_rt.append(newTup)
+in_mz_rt=sorted(in_mz_rt, key=lambda x:x[1])
+
+
+
+# x=[]
+# y=[]
+
+# for s in scan_arr:
+# 	for p in s.peaks:
+# 		x.append(s.retentionTime)
+# 		y.append(p[1])
+# plt.scatter(x,y)
+# plt.show()
+
+
