fea3c4801d620add9ca89c3aba061a8ebafa82c2
mayankmtg <mayank15056@iiitd.ac.in>
2018-05-29 16:30:40 +0530

    local slice creation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mzxmlReader import mzxmlReader as mzx
 from scan import scan
 import matplotlib.pyplot as plt
+import numpy as np
 
 file_obj=mzx('samples/file1.mzXML')
 xml_scans=file_obj.getScans()
@@ -17,12 +18,13 @@ for s in xml_scans:
 	scan_arr.append(scan_obj)
 
 print("Scans Loaded")
-in_mz_rt=[]
+mz_in_rt=[]
 for s in scan_arr:
 	for p in s.peaks:
 		newTup=(p[0], p[1], s.retentionTime)
-		in_mz_rt.append(newTup)
-in_mz_rt=sorted(in_mz_rt, key=lambda x:x[1])
+		mz_in_rt.append(newTup)
+# TODO: Check for the order of intensity and mz
+mz_in_rt=sorted(mz_in_rt, key=lambda x:x[0])
 
 
 
@@ -35,5 +37,33 @@ in_mz_rt=sorted(in_mz_rt, key=lambda x:x[1])
 # 		y.append(p[1])
 # plt.scatter(x,y)
 # plt.show()
+minmz=min(mz_in_rt, key=lambda x:x[0])
+maxmz=max(mz_in_rt, key=lambda x:x[0])
+minin=min(mz_in_rt, key=lambda x:x[1])
+maxin=max(mz_in_rt, key=lambda x:x[1])
+minrt=min(mz_in_rt, key=lambda x:x[2])
+maxrt=max(mz_in_rt, key=lambda x:x[2])
+
+looper=np.arange(minmz[0], maxmz[0], 0.1)
+slice_array=[]
+ind=0
+for l in range(1, len(looper)):
+	new_slice=[]
+	slice_mz_min=looper[l-1]
+	slice_mz_max=looper[l]
+	while(True):
+		if(mz_in_rt[ind][0]>=slice_mz_min and mz_in_rt[ind][0]<slice_mz_max):
+			new_slice.append(mz_in_rt[ind])
+			ind+=1
+		else:
+			break
+	slice_array.append(new_slice)
+mysum=0
+print(ind)
+print(len(mz_in_rt))
+for i in slice_array:
+	mysum+=len(i)
+print(mysum)
+
 
 
--- mzxmlReader.py
+++ mzxmlReader.py
@@ -15,8 +15,8 @@ class mzxmlReader():
 		mynr=base64.standard_b64decode(encoded_string)
 		peaks_ints = []
 		for i in range(0, len(mynr), 8):
-			peak = struct.unpack('>f', mynr[i:i+4])
+			mz = struct.unpack('>f', mynr[i:i+4])
 			inte = struct.unpack('>f', mynr[i+4:i+8])
-			peaks_ints.append(tuple([peak[0], inte[0]]))
+			peaks_ints.append(tuple([mz[0], inte[0]]))
 		return peaks_ints
 
